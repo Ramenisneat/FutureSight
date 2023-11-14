fc21be8570e86d10cd2d52a06f6d463a71bf3b96
ramenisneat <rahulmyana42@gmail.com>
2023-11-13 21:29:36 -0600

    model to store in db

--- ingest_ndpi_results.py
+++ ingest_ndpi_results.py
@@ -1,5 +1,21 @@
 from db import get_db
 import json
+from dataclasses import dataclass
+from typing import List
+
+
+# Although strings are inefficient, good for MVP
+@dataclass
+class device:
+    ip_addr : str
+    mac_addr: str
+    OUI_lookup: str
+    deviceType: str
+    #nmap results: ?
+    known_dests: List[str]
+    known_protos: List[str]
+    flow_risks: List[float] #ids to read from list of risks used by ndpi
+    flow_score: int
 
 
 def ingest_file(filename: str):
@@ -9,7 +25,7 @@ def ingest_file(filename: str):
     # for packet in packets:
 
 
-    print(packets[0].keys())
+    print(packets[3]["ndpi"])
 
 
 if __name__ == "__main__":
